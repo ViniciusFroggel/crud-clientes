4e9d5e20d9d826632a4c3eefc0f2d1cb6f9550b8
VineeAndre <84481899+VineeAndre@users.noreply.github.com>
2025-10-19 16:31:30 -0300

    teste 9

--- conexao.php
+++ conexao.php
@@ -1,22 +1,14 @@
 <?php
-// Dados de conexão extraídos da URL fornecida
-$host = "dpg-d3qivd0gjchc73bd95fg-a.render.com";
+$host = "dpg-d3qivd0gjchc73bd95fg-a.oregon-postgres.render.com";
 $port = "5432";
 $dbname = "crud_clientes_db";
 $user = "crud_clientes_db_user";
 $password = "izjSqf3ben7FTGwq4PNOpvUJPzB92GuP";
 
 try {
-    // Cria a conexão PDO
-   $conn = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
-
-    // Configura o PDO para lançar exceções em caso de erro
+    $conn = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    // Opcional: define o fetch padrão como associative array
     $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-
 } catch (PDOException $e) {
     die("Conexão falhou: " . $e->getMessage());
 }
-?>
